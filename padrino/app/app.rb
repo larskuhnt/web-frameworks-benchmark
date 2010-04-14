61e2a75287f0cc2cde67171d2d0b90c934075245
Nathan Esquenazi <nesquena@gmail.com>
2010-04-14 15:59:18 -0700

    Moved routes to a controller in padrino

--- padrino/app/app.rb
+++ padrino/app/app.rb
@@ -2,16 +2,4 @@ class PadrinoApp < Padrino::Application
   configure do
     enable :sessions
   end
-
-  before do
-    session[:user_id] = 10
-  end
-
-  get :index do
-    render "index"
-  end
-
-  get :about do
-    'Hello World!'
-  end
 end
