95701283e3fc17bee6ae752cb316a4ae3cb413f6
Ryan Tomayko <rtomayko@gmail.com>
2010-04-29 15:54:01 -0700

    sinatra: disable logging, static files, _method hack, and exception pages

--- sinatra/sinatra-app.rb
+++ sinatra/sinatra-app.rb
@@ -1,4 +1,11 @@
 class SinatraApp < Sinatra::Base
+  set :env, :production
+
+  disable :logging
+  disable :static
+  disable :method_override
+  disable :show_exceptions
+
   get '/' do
     content_type 'text/html'
     "Im a sinatra application"
