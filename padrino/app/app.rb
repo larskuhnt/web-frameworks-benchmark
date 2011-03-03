dea3edbb86204112e7b61f3b83add70b3dff9d43
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2011-03-03 12:54:28 -0800

    disable padrino logger

--- padrino/app/app.rb
+++ padrino/app/app.rb
@@ -1,4 +1,12 @@
 class PadrinoApp < Padrino::Application
+  disable :sessions
+  disable :flash
+  disable :logging
+  disable :padrino_logging
+  disable :static
+  disable :method_override
+  disable :show_exceptions
+
   get :index do
     "Im a padrino application"
   end
