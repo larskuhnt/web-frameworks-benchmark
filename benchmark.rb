c033f47e868496e62de36223390529d11f3fc8e0
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2011-05-05 20:10:55 +0200

    Simplify benchmarking.

--- benchmark.rb
+++ benchmark.rb
@@ -1,213 +1,106 @@
-#!/usr/bin/env ruby
+@path         = File.expand_path(File.dirname(__FILE__))
+@requests     = (ENV['N'] || 1000).to_i
+@concurrency  = (ENV['C'] || 10).to_i
+@frameworks   = %w(camping merb padrino rack rails ramaze sinatra)
 
-#
-# Runners
-#
+def run
+  puts "Testing frameworks with #{@requests} requests and #{@concurrency} connections: "
 
-module Frameworks
+  results = {}
 
-  module Ramaze
-    @port = 4000
-    @path = "ramaze"
-
-    def start
-      execute "thin start -p #{port} -d -e production"
-    end
-    def stop
-      execute "thin stop -f"
-    end
-  end
-
-  module Rails
-    @port = 4100
-    @path = "rails"
-
-    def start
-      execute "thin start -p #{port} -d -e production"
-    end
-    def stop
-      execute "thin stop -f"
-    end
-  end
-
-  module Padrino
-    @port = 4200
-    @path = 'padrino'
-
-    def start
-      execute "thin start -p #{port} -d -e production"
-    end
-    def stop
-      execute "thin stop -f"
-    end
-  end
-
-  module Sinatra
-    @port = 4300
-    @path = 'sinatra'
-
-    def start
-      execute "thin start -p #{port} -d -e production"
-    end
-    def stop
-      execute "thin stop -f"
-    end
-  end
-
-  module Merb
-    @port = 4400
-    @path = 'merb'
-
-    def start
-      execute "thin start -p #{port} -d -e production"
-    end
-    def stop
-      execute "thin stop -f"
-    end
-  end
-
-  module Rack
-    @port = 4500
-    @path = "rack"
-
-    def start
-      execute "thin start -p #{port} -d -e production"
-    end
-    def stop
-      execute "thin stop -f"
-    end
-  end
-
-  module Camping
-    @port = 4600
-    @path = 'camping'
-
-    def start
-      execute "thin start -p #{port} -d -e production"
-    end
-    def stop
-      execute "thin stop -f"
-    end
-  end
-end
-
-module Runner
-  module ClassMethods
-    attr_accessor :port, :path
-  end
-  class Base
-    attr_accessor :port, :path
-    def self.new_with(mod)
-      i = Class.new(self){ include mod }.new
-      i.extend(InstanceMethods)
-      mod.extend(ClassMethods)
-      i.path = mod.path
-      i.port = mod.port
-      i
-    end
-    def execute(str)
-      str = "cd #{path}; #{str}"
-      puts ">> #{str}"
-      system(str)
-    end
-  end
-  module InstanceMethods
-    def start
-      Dir["#{path}/log/*.log"].each { |f| File.delete(f) }
-      puts "Starting #{path} on port #{port}"
-      super
-    end
-    def stop
-      puts "Stopping #{path} on port #{port}"
-      sleep 0.3
-      super
-    end
-  end
-end
-
-requests_num = (ENV['N'] || 1000).to_i
-concurrency  = (ENV['C'] || 10).to_i
-
-#
-# Benchmark suite
-#
-runners = Frameworks.constants.map{|c| Frameworks.const_get(c)}
-# runners = [Frameworks::Padrino, Frameworks::Sinatra]
-runners.map{|r| r.extend(Runner::ClassMethods) }
-
-def run(runners, requests_num, concurrency)
-  puts "Testing frameworks with #{requests_num} requests and #{concurrency} connections: "
-  runners.each do |r|
-    puts "  #{r.name} on port #{r.port}"
-  end
-
-  results = runners.inject({}) do |table, r|
-    instance = Runner::Base.new_with(r)
-    port = r.port
-    name = r.name.gsub(/^.*::/,'')
-    puts "Benchmarking #{name} on port #{port}..."
-    cmd = %{ab -c #{concurrency} -n #{requests_num} http://127.0.0.1:#{port}/ 2>/dev/null}
-    puts ">> #{cmd}"
+  @frameworks.sort.each_with_index do |f, i|
+    port = 4000+i*10
+    puts "Benchmarking #{f} on port #{port}..."
+    cmd  = "ab -c #{@concurrency} -n #{@requests} http://127.0.0.1:#{port}/ 2>/dev/null"
+    puts "=> #{cmd}"
     result = `#{cmd}`
-    sleep 0.3
-    result[/Requests per second:\s*([\d.]+)/]
-    rps = $1.to_f
-    table[name] = rps
-    table
+    sleep 2
+    result =~ /Requests per second:\s*([\d.]+)/
+    results[f] = $1.to_f
   end
 
-  results = results.to_a.sort_by{|a| a[1]}.reverse
-  results.each do |(name, rps)|
+  puts "=" * 40
+  puts sys_info
+  puts "=" * 40
+  puts "Using:"
+  puts gems_info
+  puts "=" * 40
+  puts "Results:"
+
+  results.to_a.sort_by { |a| a[1] }.reverse.each do |name, rps|
     puts "  #{name} => #{rps} rps"
   end
 end
 
-def start(runners)
-  runners.each do |r|
-    instance = Runner::Base.new_with(r)
-    instance.start
+def start
+  @frameworks.sort.each_with_index do |f, i|
+    cmd  = "thin start -d -p #{4000+i*10} -e production -c #{File.join(@path, f)}"
+    puts "=> #{cmd}"
+    system cmd
   end
-  20.downto(0) { |i| print "Test start in #{i}s \r"; $stdout.flush; sleep 1 }
+  puts "Test will start in 20 seconds... please wait..."
+  sleep 20
 end
 
-def stop(runners)
-  runners.each do |r|
-    instance = Runner::Base.new_with(r)
-    instance.stop
+def stop
+  @frameworks.each do |f|
+    cmd = "thin stop -e production -c #{File.join(@path, f)}"
+    puts "=> #{cmd}"
+    system cmd
   end
 end
 
-cmd = ARGV[0]
+def gems_info
+  logs = Dir["**/Gemfile.lock"].map { |f| File.read(f) }.join("\n")
+  "  " + logs.scan(/((?:#{@frameworks.join("|")}){1} \([\d\.]+\))/i).flatten.uniq.join("\n  ")
+end
 
-if cmd == 'start'
-  start(runners)
-  puts ""
-  puts ""
-  sleep 0.5
-  run(runners, requests_num, concurrency)
-elsif cmd == 'restart'
-  stop(runners)
-  sleep 1
-  start(runners)
-  puts ""
-  puts ""
-  sleep 2
-  run(runners, requests_num, concurrency)
-elsif cmd == 'run'
-  run(runners, requests_num, concurrency)
-elsif cmd == 'stop'
-  stop(runners)
-elsif cmd == 'setup'
-  logs = Dir["**/Gemfile"].inject("") do |log, file|
+def sys_info
+  uname    = `uname -sr`.chomp
+  sys_info = uname =~ /darwin/i ? `system_profiler` : `cat /proc/cpuinfo` + `cat /proc/meminfo`
+  sys_info =~ /(?:Memory|MemTotal):\s+(.*)/
+  mem      = $1
+  sys_info =~ /(?:Processor Name|model name):\s+(.*)/
+  cpu_name = $1
+  sys_info =~ /(?:Processor Speed|cpu MHz):\s+(.*)/
+  cpu_mhz  = $1
+  (<<-TXT).gsub(/^ {4}/, '')
+    System: #{uname}
+    Processor: #{cpu_name} #{cpu_mhz}
+    Memory: #{mem}
+    Ruby: #{RUBY_DESCRIPTION}
+  TXT
+end
+
+def setup
+  Dir["**/*.log"].each { |f| `rm -rf #{f}` }
+  Dir["**/Gemfile"].each do |file|
     puts "=> Updating #{file}"
-    log += "\n" + `cd #{File.dirname(file)} && bundle update`.chomp
+    `cd #{File.dirname(file)}; rm -rf Gemfile.lock; bundle install`
   end
-  logs = logs.each_line.reject { |line| line !~ /camping|merb-core|padrino-core|rails|ramaze|sinatra/i || line.strip == "" }
-  puts "============================"
-  puts logs
-else
-  puts "Start and stop servers with ./benchmark.rb start|stop"
-  puts "install gems with ./benchmark.rb setup"
-  puts "Use './benchmark.rb run' against already booted servers."
-  puts ""
+end
+
+case (ARGV[0] || :help).to_sym
+  when :run
+    run
+  when :start
+    start
+    run
+  when :stop
+    stop
+  when :restart
+    stop
+    start
+    run
+  when :setup
+    setup
+  else
+    help = (<<-TXT).gsub(/^ {6}/, '')
+      Start and stop servers with:
+        $ ruby ./benchmark.rb start|stop|restart|run"
+
+      Install gems with:
+        $ ruby ./benchmark.rb setup"
+    TXT
+    puts help
 end
